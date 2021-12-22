37e756fde52eb471e9b0ee2478c38ff4fcacdd3d
burmanp <kohein@gmail.com>
2021-12-22 23:01:35 +0000

    room 25 codewar

--- main.js
+++ main.js
@@ -1,58 +1,29 @@
 /* 
 👉 Write your kata here!
  
-Check in an array if multiples of odd numbers are greater than multiples of even numbers.
+Check in an array if the multiple of odd numbers are greater than that of even numbers or vice versa.
 You may assume the array will always contain positive integers and will not be empty.
 
-Example: [1,2,3,4,5]
+Example: 
+[1,2,3,4,5]
  1*3*5 > 2*4
- Odd wins. 15 is greater than 8
+ Expected result: `Odds wins. 15 > 8`
 
- [2,3,4,6,7]
+[2,3,4,6,7]
  2*4*6 > 3*7
- Even wins(48 > 21)
- Even wins. 48 is greater than 21
-
+ Expected result: `Evens wins. 48 > 21`
+ 
 [22]
-Even wins. 22 is even
+Expected result: `Even wins. 22 is even`
 
 [19]
-Odd wins. 19 is odd
-
-
+Expected result: `Odd wins. 19 is odd`
 */
 
-
 //👉 Write the function your CodeWarriors will start with below here:
-/*Take an array of positive integers.
-  Loop through the array to find if an element is odd or even.
-  If its odd, multiply into oddTotal
-  If its even, multiply into evenTotal
-  Compare oddTotal with evenTotal
-  Return "The greater multiple is ${*Total}"*/
 
-export function isOddOrEvenMultipleWinner(arr) {
-    //Write your solution here
-
-    if (arr.length === 1){
-      if (arr[0] % 2 === 0){
-        return `Even wins. ${arr[0]} is even`;
-      }else return `Odd wins. ${arr[0]} is odd`;
-    }
-
-    let oddTotal = 1;
-    let evenTotal = 1;
-    arr.forEach(num => {
-      if (num % 2 === 0){
-        evenTotal *= num;
-      }else oddTotal *= num;
-    });
-    if (oddTotal > evenTotal) {
-      return `Odd wins. ${oddTotal} is greater than ${evenTotal}`
-    }else {
-      return `Even wins. ${evenTotal} is greater than ${oddTotal}`
-    }
+export function oddsOrEvensWinner(arr) {
+  //Write your solution here
+  //Hope you have some fun.
+  //Merry Christmas and Happy New Year
 }
-
-const result = isOddOrEvenMultipleWinner([0]);
-console.log(result)
